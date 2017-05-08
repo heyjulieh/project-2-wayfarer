20cc51fcab78f0a24c3a2c2645992f2a04e5f7c0
promethwill88 <wfong.bk@gmail.com>
2017-05-08 14:36:41 -0700

    server.js merge with addy

---
+++ wayfarer/controllers/apiControllers.js
@@ -0,0 +1,61 @@
+function index(req,res) {
+    res.json({
+        message: 'wassup Julie and Mahmoud',
+        collaborators: 'Julie, Mahmoud, Will and Addy',
+        documentation_url:'https://github.com/heyjulieh/project-2-wayfarer.git',
+        base_url: 'localhost:3000',
+        endpoint: [
+            {
+            method: 'GET',
+            path: `/api`,
+            description: 'shows this!',
+
+            },
+            {
+            method: 'GET',
+            path: `/api/city`,
+            description: 'shows all city as JSON',
+            },
+            {
+            method: 'GET',
+            path: `/api/city/${city_id}`,
+            description: 'shows req.param.id specific city as JSON',
+            },
+            {
+            method: 'GET',
+            path: `/api/posts`,
+            description: 'shows all posts as JSON',
+            },
+            {
+            method: 'GET',
+            path: `/api/${posts._id}`,
+            description: 'shows req.param.id specific lyric as JSON',
+            },
+            {
+            method: 'GET',
+            path: `/api/city/${city._id}/${posts._id}`
+            description: 'shows req.param.city and posts as JSON',
+            },
+           
+            {
+            method: 'POST',
+            path: `/api/city/${city._id}/${posts._id}`,
+            description: 'creates posts',
+            },
+            {
+            method: 'PUT',
+            path: `/api/city/${city._id}/${posts._id}`,
+            description: 'updates posts',
+            },
+            {
+            method: 'DELETE',
+            path: `/api/city/${city._id}/${posts._id}`,
+            description: 'deletes posts'
+            }
+          ]
+        });
+      }
+
+    module.exports = {
+      index: index
+    }
--- wayfarer/server.js
+++ wayfarer/server.js
@@ -1,75 +1,45 @@
 //importing dependencies
 var express = require('express'),
-	mongoose = require('mongoose'),
-	bodyParser = require('body-parser'),
-	Post = require ('.model/posts'),//for the post schema when we build one
-	City = require('.model/cities');//for the city schema whn we build one
+    mongoose = require('mongoose'),
+    db = require('./models');
+    controllers = require('./controllers');
+
+    bodyParser = require('body-parser'),
+    Post = require('./models/post'),//for the post schema when we build one
+    City = require('./models/city');//for the city schema whn we build one
 
 //to create instances
-var app = express(),
-	router = express.Router();
+var app = express()
 
 //to config API to use body body-parser and look for JSON in req.body
-app.use(bodyParser.urlencoded ({extended:true}));
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
-//set route path and initiate API
-router.get ('/', function (req,res){
-	res.json({message: 'Initilaize API'});
+//Prevent CORS errors
+app.use(function(req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Credentials', 'true');
+  res.setHeader('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT,DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers');
+
+ //Remove caching
+  res.setHeader('Cache-Control', 'no-cache');
+  next();
 });
 
-//adding /posts route to our/api router here
-router.route('posts')
-get (function (req,res) {
+app.get(`/api`, controllers.api.index);
+app.get(`/api/city`, controllers.city.index);
+app.get(`/api/city/${city_id}`, controllers.city.show);
+app.get(`/api/posts`, controllers.posts.index);
+app.get(`/api/${posts._id}`, controllers.posts.show);
+app.get(`/api/city/${city._id}/${posts._id}`, controllers.city.posts.showOne);
+app.post(`/api/city/${city._id}/${posts._id}`, controllers.city.posts.create);
+app.delete(`/api/city/${city._id}/${posts._id}`, controllers.city.posts.destroy);
+app.put(`/api/city/${city._id}/${posts._id}`, controllers.posts.update);
 
-//ADD MORE CODE HERE
-
-
-});
-
-.post(function (req,res) {
- 
- // ADD MORE CODE FOR CREATE "POST" HERE
-
-});
-
-//deleteing all posts
-router.route ('/nuke')
-.get (function (req,res) {
-
-//ADD MORE DELETE CODE HERE
-
-});
-
-// adding route for posts
-router.route(and '/cities/:cityid/post/:id')// for updating posts 
-.put ( function (req,res) {
-
-//ADD MORE CODE HERE
-
-})
-)
-
-//deleting method for removing a post from teh database
-
-.delete(function(req, res) {
-   Post.remove({ _id: req.params.post_id }, function(err, post) {
-     if (err)
-       res.send(err);
-     res.json({ message: 'Your post has been deleted' })
-   })
- });
-
-//adding route for cities
-router.route('/cities/:cityid')
-.put (function (req,res) {
-
-//ADD MORE CODE HERE PLUS DELETEING METHOD AS WELL
-
-})
 
 //use router config when we call /API
 //start server
 app.listen(port, function() {
-	console.log(`api running on ${port}`);
+    console.log(`api running on ${port}`);
 });
