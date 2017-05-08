5cd15cfade4cd8ad02435095bf148b14a98e1e0c
mahmoudbachir <mahmoud@mahmoudbachir.com>
2017-05-07 17:05:28 -0700

    Working routes

--- wayfarer/src/config/routes.js
+++ wayfarer/src/config/routes.js
@@ -1,13 +1,14 @@
 import React from 'react'
-import {Route, IndexRoute} from 'react-router'
+import {Route} from 'react-router'
 
 import Home from '../views/Home'
 import CityPostPage from '../views/CityPostPage'
 import PostPage from '../views/PostPage'
 
 module.exports = (
-  <Route path='/' component={Home}>
-    <Route path='/cities' component={CityPostPage}/>
-    <Route path='/cities/:cityname/post/:postId' component={CityPostPage}/>
-  </Route>
+	<div>
+  		<Route path='/' component={Home} />
+  		<Route path='/cities/city' component={CityPostPage}/>
+  		<Route path='/cities/city/post' component={PostPage}/>
+	</div>
 )
