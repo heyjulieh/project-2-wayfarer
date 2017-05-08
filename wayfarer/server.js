d73ff3dc40cd4b042f87c59df84a623fb11a400a
addykumar27 <adarshna27@yahoo.com>
2017-05-08 14:16:48 -0700

    adding api routes in controllers and updating them in server.js

--- wayfarer/server.js
+++ wayfarer/server.js
@@ -1,9 +1,12 @@
 //importing dependencies
 var express = require('express'),
 	mongoose = require('mongoose'),
+	var db = require('./models');
+var controllers = require('./controllers');
+
 	bodyParser = require('body-parser'),
-	Post = require ('.model/posts'),//for the post schema when we build one
-	City = require('.model/cities');//for the city schema whn we build one
+	Posts = require ('.models/posts'),//for the post schema when we build one
+	City = require('.models/city');//for the city schema whn we build one
 
 //to create instances
 var app = express(),
@@ -18,55 +21,19 @@ router.get ('/', function (req,res){
 	res.json({message: 'Initilaize API'});
 });
 
-//adding /posts route to our/api router here
-router.route('posts')
-get (function (req,res) {
+app.get(`/api`, controllers.api.index);
+app.get(`/api/city`, controllers.city.index);
+app.get(`/api/city/${city_id}`, controllers.city.show);
+app.get(`/api/posts`, controllers.posts.index);
+app.get(`/api/${posts._id}`, controllers.posts.show);
+app.get(`/api/city/${city._id}/${posts._id}`, controllers.city.posts.showOne);
 
-//ADD MORE CODE HERE
+app.post(`/api/city/${city._id}/${posts._id}`, controllers.city.posts.create);
 
+app.delete(`/api/city/${city._id}/${posts._id}`, controllers.city.posts.destroy);
 
-});
+app.put(`/api/city/${city._id}/${posts._id}`, controllers.posts.update);
 
-.post(function (req,res) {
- 
- // ADD MORE CODE FOR CREATE "POST" HERE
-
-});
-
-//deleteing all posts
-router.route ('/nuke')
-.get (function (req,res) {
-
-//ADD MORE DELETE CODE HERE
-
-});
-
-// adding route for posts
-router.route(and '/cities/:cityid/post/:id')// for updating posts 
-.put ( function (req,res) {
-
-//ADD MORE CODE HERE
-
-})
-)
-
-//deleting method for removing a post from teh database
-
-.delete(function(req, res) {
-   Post.remove({ _id: req.params.post_id }, function(err, post) {
-     if (err)
-       res.send(err);
-     res.json({ message: 'Your post has been deleted' })
-   })
- });
-
-//adding route for cities
-router.route('/cities/:cityid')
-.put (function (req,res) {
-
-//ADD MORE CODE HERE PLUS DELETEING METHOD AS WELL
-
-})
 
 //use router config when we call /API
 //start server
