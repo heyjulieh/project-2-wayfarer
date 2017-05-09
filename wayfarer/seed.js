668a15f8357daf46fa1bae4c4c43770bd005b60f
promethwill88 <wfong.bk@gmail.com>
2017-05-08 18:58:33 -0700

    modified posts seed data

--- wayfarer/seed.js
+++ wayfarer/seed.js
@@ -5,36 +5,75 @@ var cities_list = [
 		cityName: 'San Francisco',
 		imgURL: '/images/cities/sanfrancisco.jpg',
 		type: 'largeCard',
-		posts: 'A wonderful city by the water'
+		posts: 
+			{
+			userImg: '',
+			user: 'Will Fong',
+			cityName: 'San Francisco',
+			title: 'While My Guitar Gently Weeps',
+			text: 'The shrooms were magnificient. This city sure knows how to fly!',
+			date: '3/25/2017'
+			}
 	},
 	{
 		cityName: 'Dubai',
 		imgURL: '/images/cities/dubai.jpg',
 		type: 'mediumCard',
-		posts: 'A wonderful city in the desert'
+		posts: 
+			{
+			userImg: '',
+			user: 'Addy Kumar',
+			cityName: 'Dubai',
+			title: 'Yesterday',
+			text: 'Its too damn hot here.',
+			date: '1/2/2017'
+			}
 	},
 	{
 		cityName: 'Reykjavik',
 		imgURL: '/images/cities/reykjavik.jpg',
 		type: 'smallCard',
-		posts: 'A wonderful city in the tundra'
+		posts: 
+			{
+			userImg: '',
+			user: 'Julie Huang',
+			cityName: 'Reykjavik',
+			title: 'Hold My Hand',
+			text: 'COLD COLD COLD, but the hot springs are a must see.',
+			date: '4/10/2016'
+			}
 	},
 	{
 		cityName: 'Tokyo',
 		imgURL: '/images/cities/tokyo.jpg',
 		type: 'smallCard',
-		posts: 'A wonderful city in the Southeast'
+		posts: 
+			{
+			userImg: '',
+			user: 'Julie Huang',
+			cityName: 'Reykjavik',
+			title: 'Hold My Hand',
+			text: 'COLD COLD COLD, but the hot springs are a must see.',
+			date: '4/10/2016'
+			}
 	},
 	{
 		cityName: 'Amsterdam',
 		imgURL: '/images/cities/amsterdam.jpg',
 		type: 'largeCard',
-		posts: 'A wonderful city by the canals'
+		posts: 
+			{
+			userImg: '',
+			user: 'Mahmoud Bachir',
+			cityName: 'Tokyo',
+			title: 'Lucy In The Sky With Diamonds',
+			text: 'Ramen was so bomb!',
+			date: '3/25/2017'
+			}
 	}
 ]
 
-
-db.City.remove({}, function(err,cities) {
+db.City.remove({}, function(err, cities) {
 	console.log('removed all cities')
 	db.City.create(cities_list, function(err, cities) {
 		if (err){
