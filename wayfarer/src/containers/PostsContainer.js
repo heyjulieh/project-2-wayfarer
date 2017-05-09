dc5d4fc40c59a950c02f47d07fec60870080220e
promethwill88 <wfong.bk@gmail.com>
2017-05-08 22:00:16 -0700

    removed hard coded data, get ajax call

--- wayfarer/src/containers/PostsContainer.js
+++ wayfarer/src/containers/PostsContainer.js
@@ -8,37 +8,22 @@ class PostsContainer extends Component {
 	constructor() {
 		super();
 		this.state = {
-			posts: [
-			{	_id: 1, 
-				userIMG: 'https://images.pexels.com/photos/101584/pexels-photo-101584.jpeg?w=1260&h=750&auto=compress&cs=tinysrgb', 
-				user: 'Anthony L.', 
-				title: 'Home is Where the Sandwich is', 
-				text: 'This is a sample text entry',
-				date: '5/10/2017'
-			},
-			{	_id: 2, 
-				userIMG: 'https://images.pexels.com/photos/27411/pexels-photo-27411.jpg?w=1260&h=750&auto=compress&cs=tinysrgb', 
-				user: 'Violet', 
-				title: 'Words from a Local', 
-				text: 'This is a sample text entry',
-				date: '5/10/2017'
-			},
-			{	_id: 3, 
-				userIMG: 'https://images.pexels.com/photos/91227/pexels-photo-91227.jpeg?w=1260&h=750&auto=compress&cs=tinysrgb', 
-				user: 'Blair', 
-				title: 'Really Cool Vibes, Man.', 
-				text: 'This is a sample text entry',
-				date: '5/10/2017'
-			},
-
-
-
-
-				]
-			// need to pull in all Post objects data from API
+			posts: []
 		}
-
+		this.loadPostsFromServer = this.loadPostsFromServer.bind(this);
 		this.handleNewPostSubmit = this.handleNewPostSubmit.bind(this);
+	}
+
+	loadPostsFromServer() {
+		$.ajax({
+			method: 'GET',
+			url: 'http://localhost:3000/api/cities'
+		})
+		.then( res => this.setState({cities: res}))
+	}
+
+	componentDidMount() {
+		this.loadPostsFromServer();
 	}
 
 	handleNewPostSubmit(post){
@@ -64,3 +49,29 @@ class PostsContainer extends Component {
 }
 
 export default PostsContainer;
+
+// {	_id: 1, 
+// 	userIMG: 'https://images.pexels.com/photos/101584/pexels-photo-101584.jpeg?w=1260&h=750&auto=compress&cs=tinysrgb', 
+// 	user: 'Anthony L.', 
+// 	title: 'Home is Where the Sandwich is', 
+// 	text: 'This is a sample text entry',
+// 	date: '5/10/2017'
+// },
+// {	_id: 2, 
+// 	userIMG: 'https://images.pexels.com/photos/27411/pexels-photo-27411.jpg?w=1260&h=750&auto=compress&cs=tinysrgb', 
+// 	user: 'Violet', 
+// 	title: 'Words from a Local', 
+// 	text: 'This is a sample text entry',
+// 	date: '5/10/2017'
+// },
+// {	_id: 3, 
+// 	userIMG: 'https://images.pexels.com/photos/91227/pexels-photo-91227.jpeg?w=1260&h=750&auto=compress&cs=tinysrgb', 
+// 	user: 'Blair', 
+// 	title: 'Really Cool Vibes, Man.', 
+// 	text: 'This is a sample text entry',
+// 	date: '5/10/2017'
+// },
+
+
+
+
