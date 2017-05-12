e18b9f5620c84341289be458d52899f259d5e745
heyjulieh <heyjulieh@gmail.com>
2017-05-12 12:35:00 -0700

    updated navbar linking and some other css

--- wayfarer/src/components/Post.js
+++ wayfarer/src/components/Post.js
@@ -3,32 +3,30 @@ import React, {Component} from 'react'
 
 class Post extends Component {
 
+    render() {
+
+        let formattedDate = this.props.post.date.split("T")[0];
+        let truncatedText = this.props.post.text.substring(0, 350) + '…';
+        let postLink = `/cities/${this.props.post.city}/posts/${this.props.post._id}`
 
 
-	render() {
+        return(
 
-		let formattedDate = this.props.post.date.split("T")[0];
-		let truncatedText = this.props.post.text.substring(0, 350) + '…';
-		let postLink = `/cities/${this.props.post.city}/posts/${this.props.post._id}`
+            <div className="postCard col-sm-12 col-md-12 col-lg-12">
+                <div className="userSection col-sm-12 col-md-12 col-lg-3">
+                    <img className="postImage img-circle img-responsive center-block" src={this.props.post.userIMG}></img>
+                    <h3 className="postUser">{this.props.post.user}</h3>
+                </div>
+                <div className="postSection col-sm-12 col-md-12 col-lg-9">
+                    <h1 className="postTitle">{this.props.post.title}</h1>
+                    <p className="postText">{truncatedText}</p>
+                    <h5><a href={postLink}>Read more…</a></h5>
+                    <h6 className="postDate">Posted on: {formattedDate}</h6>
 
-
-		return(
-
-			<div className="postCard col-sm-12 col-md-12 col-lg-12">
-				<div className="userSection col-sm-12 col-md-12 col-lg-3">
-					<img className="postImage img-circle img-responsive center-block" src={this.props.post.userIMG}></img>
-					<h3 className="postUser">{this.props.post.user}</h3>
-				</div>
-				<div className="postSection col-sm-12 col-md-12 col-lg-9">
-					<h1 className="postTitle">{this.props.post.title}</h1>
-					<p className="postText">{truncatedText}</p>
-					<h5><a href={postLink}>Read more…</a></h5>
-					<h6 className="postDate">Posted on: {formattedDate}</h6>
-
-				</div>
-			</div>
-		)
-	}
+                </div>
+            </div>
+        )
+    }
 }
 
 export default Post;
