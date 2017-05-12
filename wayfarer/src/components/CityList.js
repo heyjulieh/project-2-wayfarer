300a3c4e620af7a105dddbf2b33bff305cbc024e
mahmoudbachir <mahmoud@mahmoudbachir.com>
2017-05-12 07:19:29 -0700

    hot fix for broken footer positioning on detail page

--- wayfarer/src/components/CityList.js
+++ wayfarer/src/components/CityList.js
@@ -19,6 +19,8 @@ class CityList extends Component {
 
 			<div className="citiesGrid container-fluid">
 				<div className="row">
+				<h1 className="center-text">Explore Cities</h1>
+
 					{citiesArray}
 				</div>
 			</div>
